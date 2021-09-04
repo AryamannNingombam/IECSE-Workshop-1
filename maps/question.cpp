2fc9949f5a413e0c9019da02a2e0ae8bbe5b77fb
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-09-04 11:13:45 +0530

    added comments

--- maps/question.cpp
+++ maps/question.cpp
@@ -18,32 +18,56 @@ int lengthOfLongestSubstring(string s)
     if (s.size() == 0)
         return 0;
 
-    //if we had not kept the previous check, this would throw an error
-    //(segmentation fault;)
+    //incrementing for the first element;
     count[s[0]]++;
+
+    //the minimum answer would be one
+    //(if there is only one character in the whole string)
     int answer{1};
-    int i{}, j{};
+
+    //i would be the first pointer, j would be the second pointer;
+    int i{0}, j{0};
+
+    //while we don't reach the end of the string;
     while (j != s.size() - 1)
     {
+        //if the number of times the next character has
+        //occured between the first pointer and the 
+        //second pointer is zero;
         if (count[s[j + 1]] == 0)
         {
+            //incrementing this (this character has been found)
             count[s[j + 1]]++;
+            //incrementing the second pointer, to move forward;
             j++;
+
+            //this would be the final answer, the max would be kept
+            //as we are asked to display the length of the longest 
+            //substring;
             answer = max(answer, j - i + 1);
         }
         else
         {
+            //this would happen if the next character after the
+            //second pointer has been encountered before in the window,
+            //we would move the first pointer forward until the 
+            //particular character is not removed from the window;
             count[s[i]]--;
             i++;
         };
     };
+
+    //returning the final answer;
     return answer;
 }
 
 int main()
 {
+
     string s;
     cin >> s;
+
+    //the answer printed;
     cout << lengthOfLongestSubstring(s) << '\n';
 
     return 0;
--- maps/question2.cpp
+++ maps/question2.cpp
@@ -1,60 +1,97 @@
-// StlGod.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
- 
-// #include "pch.h"
 #include <iostream>
 #include <map>
 #include <unordered_map>
 #include <vector>
- 
+
 using namespace std;
- 
+
 int main()
 {
 	int n;
 	cin >> n;
-	vector<int> A(n+5, 0);
+	//n+5 is kept just to avoid any segmentation errors,
+	//this is not necessary;
+	vector<int> A(n + 5, 0);
+
+	//long long is used as the constraints that the question has
+	//on the numbers, the sum might exceed the upper limit of int;
 	long long S = 0;
-	for (int i = 0; i < n; i++) cin >> A[i], S += A[i];
-	if (S & 1) {
+
+	//taking the input and storing it in the vector,
+	//simultaneously getting the sum;
+	for (int i = 0; i < n; i++)
+		cin >> A[i], S += A[i];
+
+	//the first check, if the sum is odd, then a division
+	//cannot happen, because the data type is not a float/double;
+	if (S & 1)
+	{
 		cout << "NO\n";
 		return 0;
 	}
+
+	//the first map would take care of the
+	//first half, the second map would take care
+	//of the second half;
 	unordered_map<long long, int> first, second;
- 
+
+	//the first half would initially consist of
+	//only the first character;
 	first[A[0]] = 1;
- 
-	for (int i = 1; i < n; i++) second[A[i]]++;
- 
+
+	//filling the second half with the appropriate
+	//values;
+	for (int i = 1; i < n; i++)
+		second[A[i]]++;
+
+	//this would be the sum of the first half, the
+	//value of this variable would decide
+	//the flow of the code;
 	long long sdash = 0;
- 
-	for (int i = 0; i < n ; i++) {
+
+	for (int i = 0; i < n; i++)
+	{
 		sdash += A[i]; //first i numbers
-		if (sdash == S / 2) {
+
+		//if the first half is
+		//equal to s/2, the second half would automatically
+		//be equal to s/2;
+		if (sdash == S / 2)
+		{
 			cout << "YES\n";
 			return 0;
 		}
-		if (sdash < S / 2) {
+
+		//if sdash is lesser, it means that we should search for an
+		//element in the second half, that is equal to the
+		//difference between s/2 & sdash;
+		if (sdash < S / 2)
+		{
 			long long x = S / 2 - sdash;
-			//delete element from second half, and insert into first half
-			if (second[x] > 0) {
+			//the check;
+			if (second[x] > 0)
+			{
 				cout << "YES\n";
 				return 0;
 			}
 		}
-		else {
+		//if the sum is larger, then find an element in the first 
+		//half which is equal to sdash - s/2;
+		else
+		{
 			long long y = sdash - S / 2;
-			if (first[y] > 0) {
+
+			//the check;
+			if (first[y] > 0)
+			{
 				cout << "YES\n";
 				return 0;
 			}
 		}
 		//i+1
-		first[A[i+1]]++;
+		first[A[i + 1]]++;
 		second[A[i + 1]]--;
 	}
- 
+
 	cout << "NO\n";
- 
- 
 }
